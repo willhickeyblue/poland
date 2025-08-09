b5965ad6c91752fa618175adba67c8ab4d0213c8
willhickeyblue <kirbpuffrocket@gmail.com>
2025-08-08 23:05:23 -0700

    Update poland.pu

--- poland.py
+++ poland.py
@@ -1,10 +1,15 @@
 # ok so i made this on a crappy phone so dont expect ts to be good or work
 import moviepy
-video = "/ScreenRecording_08-08-2025 21-36-28_1.mov"
+video = VideoFileClip("./ScreenRecording_08-08-2025 21-36-28_1.mov")
 words = ["blue", "red", "green", "economically viable", "conservative", "liberal", "evil", "sigma", "brainrotted", "Messi", "Canadian", "European", "Playboi Carti", "British", "small content creator", "TikTok", "male", "crazy", "forsaken fan", "roblox fan", "fnaf", "environmentally friendly", "unemployed", "American", "rapper"]
 for likeword in words:
     for commentword in words:
         zetext = TextClip{
             text = f"which is better: \n {likeword}: like the video \n or \n {commentword}: comment on the video \n \n im trying to revive this sound, \n remix the video to help me!"
         }
+        final = CompositeVideoClip(
+            [video, zetext]
+        )
+        final.write_videofile(f"./{likeword}{commentword}")
+    
         
