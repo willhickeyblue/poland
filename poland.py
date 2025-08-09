e978ef7e580d43bf8a2ca6f8043ac44e6f72afd4
willhickeyblue <kirbpuffrocket@gmail.com>
2025-08-08 21:17:12 -0700

    Words words words

--- poland.py
+++ poland.py
@@ -1,1 +1,4 @@
 # ok so i made this on a crappy phone so don't expect ts to be good or work
+import moviepy
+
+words = ["blue", "red", "green", "economically viable", "conservative", "liberal", "evil", "sigma", "brainrotted", "Messi", "Canadian", "European", "Playboi Carti", "British", "small content creator", "TikTok", "male", "crazy", "forsaken fan", "roblox fan", "fnaf", "environmentally friendly", "unemployed", "American", "rapper"]
