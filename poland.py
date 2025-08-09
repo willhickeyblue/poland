ae75b2c982851352f049eb052f73c6f489f98745
willhickeyblue <kirbpuffrocket@gmail.com>
2025-08-08 21:30:09 -0700

    Zetext

--- poland.py
+++ poland.py
@@ -1,4 +1,10 @@
-# ok so i made this on a crappy phone so don't expect ts to be good or work
+# ok so i made this on a crappy phone so dont expect ts to be good or work
 import moviepy
-
+video = "/movie.mp4"
 words = ["blue", "red", "green", "economically viable", "conservative", "liberal", "evil", "sigma", "brainrotted", "Messi", "Canadian", "European", "Playboi Carti", "British", "small content creator", "TikTok", "male", "crazy", "forsaken fan", "roblox fan", "fnaf", "environmentally friendly", "unemployed", "American", "rapper"]
+for likeword in words:
+    for commentword in words:
+        zetext = TextClip{
+            text = f"which is better: \n {likeword}: like the video \n or \n {commentword}: comment on the video \n \n im trying to revive this sound, \n remix the video to help me!"
+        }
+        
