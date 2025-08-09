0e9c6dd68720e00e71427333b1a3fcc59b6b666c
willhickeyblue <kirbpuffrocket@gmail.com>
2025-08-08 22:49:56 -0700

    Update poland.py

--- poland.py
+++ poland.py
@@ -1,6 +1,6 @@
 # ok so i made this on a crappy phone so dont expect ts to be good or work
 import moviepy
-video = "/movie.mp4"
+video = "/ScreenRecording_08-08-2025 21-36-28_1.mov"
 words = ["blue", "red", "green", "economically viable", "conservative", "liberal", "evil", "sigma", "brainrotted", "Messi", "Canadian", "European", "Playboi Carti", "British", "small content creator", "TikTok", "male", "crazy", "forsaken fan", "roblox fan", "fnaf", "environmentally friendly", "unemployed", "American", "rapper"]
 for likeword in words:
     for commentword in words:
